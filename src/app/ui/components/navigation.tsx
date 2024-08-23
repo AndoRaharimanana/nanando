a8c4e2d4621139584cc6b9f3d95004006ecb193a
AndoRaharimanana <raharimanana99@gmail.com>
2024-08-23 23:10:55 +0300

    responsive menu

--- src/app/ui/components/navigation.tsx
+++ src/app/ui/components/navigation.tsx
@@ -1,6 +1,8 @@
-import { Poppins} from "../font";
+import React, {useState} from 'react';
+import { Poppins, PoppinsSemiBold, Lora} from "../font";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
+import SocialLinks from "./socialLinks";
 
 function Menu() {
     return (
@@ -16,18 +18,35 @@ function Menu() {
 }
 
 export default function Navigation() {
+    const [displayMobileMenu, setDisplayMobileMenu] = useState(false);
+    const clickMobileMenu = () => {
+        setDisplayMobileMenu(!displayMobileMenu);
+    };    
     return (
         <>
             <ul className={`rotate-180 ${Poppins.className} gap-[50px] pl-[25px] flex-row-reverse [writing-mode:vertical-lr] [text-orientation: mixed] fixed hidden lg:flex`}>
                 <Menu />
             </ul>            
             <div className="fixed lg:hidden mx-4 z-50">
-                <FontAwesomeIcon icon={faBars} fontSize={25}/>
+                <FontAwesomeIcon icon={displayMobileMenu ? faXmark : faBars} fontSize={25} onClick={clickMobileMenu}/>
             </div>            
-            <div className="fixed w-full h-full bg-nanando-soft-grey z-40 top-0">
-                <ul className="">
-                    <Menu />
-                </ul>
+            <div className={`fixed ${displayMobileMenu ? '' : 'hidden'} lg:hidden w-full h-full bg-nanando-soft-grey z-40 top-0 flex items-center justify-center`}>
+                <div className="flex flex-col md:flex-row items-center justify-around gap-6 md:w-full">
+                    <div className="flex">
+                        <ul className={`leading-[80px] text-3xl ${PoppinsSemiBold.className}`}>
+                            <Menu />
+                        </ul>
+                    </div>
+                    <div className="flex flex-col gap-4">
+                        <ul className={`leading-6 ${Lora.className}`}>
+                            <li className="">aanod@email.com</li>
+                            <li className="">+2613404040</li>
+                        </ul>                
+                        <div>
+                            <SocialLinks />
+                        </div>
+                    </div>
+                </div>
             </div> 
         </>
     );
