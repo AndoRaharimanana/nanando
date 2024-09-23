cd02e3074cb8279a86c64d0800718ea53b926b88
AndoRaharimanana <raharimanana99@gmail.com>
2024-09-23 22:48:36 +0300

    fix prod

--- src/app/api/download/route.tsx
+++ src/app/api/download/route.tsx
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import fs from 'fs';
+import fs from 'fs/promises';
 import path from 'path';
 
 export async function GET(request: Request) {
@@ -7,28 +7,30 @@ export async function GET(request: Request) {
   const fileName = 'Ando-Raharimanana-CV.pdf';
 
   // Check if the file exists
-  if (!fs.existsSync(filePath)) {
+  try {
+    const file = await fs.readFile(filePath);
+    
+    // Define a mapping of file extensions to content types
+    const contentTypeMap: { [key: string]: string } = {
+      svg: 'image/svg+xml',
+      ico: 'image/x-icon',
+      png: 'image/png',
+      jpg: 'image/jpeg',
+      pdf: 'application/pdf',
+    };
+
+    // Get the file extension
+    const fileExtension = fileName.split('.').pop()?.toLowerCase() || '';
+    const contentType = contentTypeMap[fileExtension] || 'application/octet-stream';
+
+    // Set headers to force download
+    const headers = new Headers({
+      'Content-Disposition': `attachment; filename="${fileName}"`,
+      'Content-Type': contentType,
+    });
+
+    return new Response(file, { headers });
+  } catch (error) {
     return NextResponse.json({ error: 'File not found' }, { status: 404 });
   }
-
-  // Define a mapping of file extensions to content types
-  const contentTypeMap: { [key: string]: string } = {
-    pdf: 'application/pdf',
-  };
-
-  // Get the file extension
-  const fileExtension = fileName.split('.').pop()?.toLowerCase() || '';
-  const contentType = contentTypeMap[fileExtension] || 'application/octet-stream';
-
-  // Set headers to force download
-  const headers = new Headers({
-    'Content-Disposition': `attachment; filename="${fileName}"`,
-    'Content-Type': contentType,
-  });
-
-  const fileStream = fs.createReadStream(filePath);
-
-  return new Response(fileStream, {
-    headers,
-  });
 }
