70d57ff4c05c97b7f93368a5e451195ae4fa163b
AndoRaharimanana <raharimanana99@gmail.com>
2025-06-23 22:38:12 +0300

    update years

--- src/app/services/data.tsx
+++ src/app/services/data.tsx
@@ -1,349 +1,354 @@
-  const data =  {
-    chronologie: [
-      {
-        year: 'April 2023 - by now',
-        poste: 'Remote Fullstack Symfony/React/React Native',
-        company: {
-          logo: '/ifoxcode.png',
-          name: 'Ifoxcode',
-          place: 'Mauritius',
-          link: "https://ifoxcode.com/"
-        },
-        description: "I spent 1.5 years as a full-stack mobile developer at iFoxCode (Mauritius), working remotely. My role involved building Android and iOS mobile apps using React Native, developing backend APIs with Symfony, and redesigning and creating web applications with React. My experience there allowed me to contribute to diverse projects, handling both frontend and backend tasks to deliver complete solutions on several projects.",
-        projects: [
-          {
-            name: "AlphaPest Management",
-            logo: "/projects/alphapest.png"
-          },
-          {
-            name: "Burger King - Chatbot",
-            logo: "/projects/Burger-King.png"
-          },
-          {
-            name: "IfoxCode News",
-            logo: "/projects/ifoxcode.jpg"
-          },
-          {
-            name: "Luxury Avenue",
-            logo: "/projects/luxury-avenue.png"
-          },
-          {
-            name: "MaxCity",
-            logo: "/projects/maxcity.png"
-          },
-          {
-            name: "RSE Datanews",
-            logo: "/projects/rdn.png"
-          },
-          {
-            name: "Terminal Management System",
-            logo: "/projects/tms.png"
-          },
-          {
-            name: "BussinessLink",
-            logo: "/projects/businesslink.svg"
-          },
-          {
-            name: "Digital Marketing",
-            logo: "/projects/digitalmarketing.svg"
-          },
-          {
-            name: "Kaktus",
-            logo: "/projects/kaktus.png"
-          },
-          {
-            name: "Sesame",
-            logo: "/projects/sesame.png"
-          }
-        ]
+const data = {
+  chronologie: [
+    {
+      year: "April 2023 - by now",
+      poste: "Remote Fullstack Symfony/React/React Native",
+      company: {
+        logo: "/ifoxcode.png",
+        name: "Ifoxcode",
+        place: "Mauritius",
+        link: "https://ifoxcode.com/",
       },
-      {
-        year: 'December 2021 - by now',
-        poste: 'JAVA Developer',
-        company: {
-          logo: '/bici.png',
-          name: 'BICI',
-          place: 'Madagascar',
-          link: "https://bici.mg/"
-        },
-        description: "I spent 3 years as a Java developer at BICI, working on-site. My responsibilities included debugging and fixing issues for web applications, developing backend solutions, and reporting customer feedback. I also proposed solutions based on this feedback and trained customers on how to effectively use the applications. This experience sharpened my problem-solving skills and deepened my expertise in backend development.",
-        projects: [
-          {
-            name: "Agence de transport terrestres (ATT)",
-            logo: "/projects/att.jpg"
-          },
-          {
-            name: "La Société du Port à gestion Autonome de Toamasina (S.P.A.T)",
-            logo: "/projects/spat.png"
-          },
-          {
-            name: "Pho Resto",
-            logo: "/projects/pho.jpg"
-          },
-          {
-            name: "Fonds Routier Madagascar",
-            logo: "/projects/fer.png"
-          },
-          {
-            name: "Team Task"
-          }
-        ]
-      },
-      {
-        year: 'July 2023 - October 2023',
-        poste: 'Intern Blockchain Developer',
-        company: {
-          logo: '/bici.png',
-          name: 'BICI',
-          place: 'Madagascar',
-          link: "https://bici.mg/"
-        },
-        description: "During my 3-month internship at BICI as a Blockchain Developer, I worked on creating a cryptocurrency using blockchain technology. My responsibilities included developing a website and wallet integrated with MetaMask, ensuring smooth transaction processes, and building functionalities for minting currency. This experience enhanced my understanding of blockchain applications and cryptocurrency development.",
-        projects: [
-          {
-            name: "Madacoin"
-          }
-        ]
-      },
-      {
-        year: 'December 2021 - March 2023',
-        poste: 'Remote Webmaster',
-        company: {
-          logo: '/rapp.png',
-          name: 'RAPP',
-          place: 'Mauritius',
-          link: "https://www.rapp.com/"
-        },
-        description: "At RAPP Mauritius, I worked remotely as a Webmaster for over a year. My role involved managing the content of Mercedes-Benz websites across multiple countries (France, UK, Japan, Argentina, Switzerland, and others). I was responsible for troubleshooting, proposing solutions for bugs, and assisting customers with updates. This experience honed my ability to manage content at scale and provide effective client support.",
-        projects: [
-          {
-            name: "Mercedes-Benz OneWeb",
-            logo: "/projects/mercedes.png"
-          }
-        ]
-      },
-      {
-        year: 'August 2021 - November 2021',
-        poste: 'Intern JAVA Developer',
-        company: {
-          logo: '/bici.png',
-          name: 'BICI',
-          place: 'Madagascar',
-          link: "https://bici.mg/"
-        },
-        description: "During my 3-month internship at BICI, I worked on-site as a Java Developer. My responsibilities included debugging and fixing issues for web applications, proposing and developing new functionalities for the internal team task management system. This experience helped me enhance my problem-solving skills and contribute to team projects with innovative solutions.",
-        projects: [
-          {
-            name: "La Société du Port à gestion Autonome de Toamasina (S.P.A.T)",
-            logo: "/projects/spat.png"
-          },
-          {
-            name: "Fonds Routier Madagascar",
-            logo: "/projects/fer.png"
-          },
-          {
-            name: "Team Task"
-          }
-        ]
-      },
-    ],
-    backEndData: [
-      {
-        name: 'Php',
-        value: 90,
-      },
-      {
-        name: 'Java',
-        value: 80,
-      },
-      {
-        name: 'C#',
-        value: 60,
-      },
-      {
-        name: 'SQL',
-        value: 80,
-      },
-      {
-        name: 'NoSQL',
-        value: 60,
-      },
-      {
-        name: 'Symfony',
-        value: 90,
-      },
-      {
-        name: 'Ethereum',
-        value: 75,
-      },
-      {
-        name: 'NodeJS',
-        value: 75,
-      }
-    ],
-    frontEndData: [
-      {
-        name: 'HTML/CSS',
-        value: 95,
-      },
-      {
-        name: 'JavaScript',
-        value: 90,
-      },
-      {
-        name: 'React',
-        value: 89,
-      },
-      {
-        name: 'React Native',
-        value: 90,
-      },
-      {
-        name: 'Tailwind',
-        value: 75,
-      },
-      {
-        name: 'Sass',
-        value: 85,
-      },
-      {
-        name: 'JQuery',
-        value: 80,
-      }
-    ],
-    additionalSkillsData: {
-      withPhoto: [
+      description:
+        "I spent 2 years as a full-stack mobile developer at iFoxCode (Mauritius), working remotely. My role involved building Android and iOS mobile apps using React Native, developing backend APIs with Symfony, and redesigning and creating web applications with React. My experience there allowed me to contribute to diverse projects, handling both frontend and backend tasks to deliver complete solutions on several projects.",
+      projects: [
         {
-          name: 'Visual Code',
-          logo: "/skills/visualcode.png",
+          name: "AlphaPest Management",
+          logo: "/projects/alphapest.png",
         },
         {
-          name: 'Expo',
-          logo: "/skills/expo.png",
+          name: "Burger King - Chatbot",
+          logo: "/projects/Burger-King.png",
         },
         {
-          name: 'Github',
-          logo: "/skills/github.png",
+          name: "IfoxCode News",
+          logo: "/projects/ifoxcode.jpg",
         },
         {
-          name: 'Docker',
-          logo: "/skills/docker.png",
+          name: "Luxury Avenue",
+          logo: "/projects/luxury-avenue.png",
         },
         {
-          name: 'Firebase',
-          logo: "/skills/firebase.png",
+          name: "MaxCity",
+          logo: "/projects/maxcity.png",
         },
         {
-          name: 'Postman',
-          logo: "/skills/postman.png",
+          name: "RSE Datanews",
+          logo: "/projects/rdn.png",
         },
         {
-          name: 'MySQL',
-          logo: "/skills/mysql.png",
+          name: "Terminal Management System",
+          logo: "/projects/tms.png",
         },
         {
-          name: 'Postgresql',
-          logo: "/skills/postgresql.png",
+          name: "BussinessLink",
+          logo: "/projects/businesslink.svg",
         },
         {
-          name: 'MongoDB',
-          logo: "/skills/mongodb.png",
+          name: "Digital Marketing",
+          logo: "/projects/digitalmarketing.svg",
         },
         {
-          name: 'NextJS',
-          logo: "/skills/nextjs.png",
+          name: "Kaktus",
+          logo: "/projects/kaktus.png",
         },
         {
-          name: 'Wordpress',
-          logo: "/skills/wordpress.png",
-        } 
+          name: "Sesame",
+          logo: "/projects/sesame.png",
+        },
       ],
-      noPhoto: [
-        {
-          name: 'JWT/Basic/Token Auth',          
-        },
-        {
-          name: 'RESTful APIs',
-        },
-        {
-          name: 'JSON APIs',
-        },
-        {
-          name: 'ORMs',
-        },
-        {
-          name: 'Microservices',
-        },
-        {
-          name: 'Responsive Design',
-        },
-        {
-          name: 'DOM Manipulation',
-        },
-        {
-          name: 'Fecth API/Ajax',
-        },
-        {
-          name: 'Package Managers(npm)',
-        },
-        {
-          name: 'TypeScript',
-        },
-        {
-          name: 'Blockchain',
-        }
-      ]     
     },
-    formation: [
+    {
+      year: "December 2021 - by now",
+      poste: "JAVA Developer",
+      company: {
+        logo: "/bici.png",
+        name: "BICI",
+        place: "Madagascar",
+        link: "https://bici.mg/",
+      },
+      description:
+        "I spent 3 years as a Java developer at BICI, working on-site. My responsibilities included debugging and fixing issues for web applications, developing backend solutions, and reporting customer feedback. I also proposed solutions based on this feedback and trained customers on how to effectively use the applications. This experience sharpened my problem-solving skills and deepened my expertise in backend development.",
+      projects: [
+        {
+          name: "Agence de transport terrestres (ATT)",
+          logo: "/projects/att.jpg",
+        },
+        {
+          name: "La Société du Port à gestion Autonome de Toamasina (S.P.A.T)",
+          logo: "/projects/spat.png",
+        },
+        {
+          name: "Pho Resto",
+          logo: "/projects/pho.jpg",
+        },
+        {
+          name: "Fonds Routier Madagascar",
+          logo: "/projects/fer.png",
+        },
+        {
+          name: "Team Task",
+        },
+      ],
+    },
+    {
+      year: "July 2023 - October 2023",
+      poste: "Intern Blockchain Developer",
+      company: {
+        logo: "/bici.png",
+        name: "BICI",
+        place: "Madagascar",
+        link: "https://bici.mg/",
+      },
+      description:
+        "During my 3-month internship at BICI as a Blockchain Developer, I worked on creating a cryptocurrency using blockchain technology. My responsibilities included developing a website and wallet integrated with MetaMask, ensuring smooth transaction processes, and building functionalities for minting currency. This experience enhanced my understanding of blockchain applications and cryptocurrency development.",
+      projects: [
+        {
+          name: "Madacoin",
+        },
+      ],
+    },
+    {
+      year: "December 2021 - March 2023",
+      poste: "Remote Webmaster",
+      company: {
+        logo: "/rapp.png",
+        name: "RAPP",
+        place: "Mauritius",
+        link: "https://www.rapp.com/",
+      },
+      description:
+        "At RAPP Mauritius, I worked remotely as a Webmaster for over a year. My role involved managing the content of Mercedes-Benz websites across multiple countries (France, UK, Japan, Argentina, Switzerland, and others). I was responsible for troubleshooting, proposing solutions for bugs, and assisting customers with updates. This experience honed my ability to manage content at scale and provide effective client support.",
+      projects: [
+        {
+          name: "Mercedes-Benz OneWeb",
+          logo: "/projects/mercedes.png",
+        },
+      ],
+    },
+    {
+      year: "August 2021 - November 2021",
+      poste: "Intern JAVA Developer",
+      company: {
+        logo: "/bici.png",
+        name: "BICI",
+        place: "Madagascar",
+        link: "https://bici.mg/",
+      },
+      description:
+        "During my 3-month internship at BICI, I worked on-site as a Java Developer. My responsibilities included debugging and fixing issues for web applications, proposing and developing new functionalities for the internal team task management system. This experience helped me enhance my problem-solving skills and contribute to team projects with innovative solutions.",
+      projects: [
+        {
+          name: "La Société du Port à gestion Autonome de Toamasina (S.P.A.T)",
+          logo: "/projects/spat.png",
+        },
+        {
+          name: "Fonds Routier Madagascar",
+          logo: "/projects/fer.png",
+        },
+        {
+          name: "Team Task",
+        },
+      ],
+    },
+  ],
+  backEndData: [
+    {
+      name: "Php",
+      value: 90,
+    },
+    {
+      name: "Java",
+      value: 80,
+    },
+    {
+      name: "C#",
+      value: 60,
+    },
+    {
+      name: "SQL",
+      value: 80,
+    },
+    {
+      name: "NoSQL",
+      value: 60,
+    },
+    {
+      name: "Symfony",
+      value: 90,
+    },
+    {
+      name: "Ethereum",
+      value: 75,
+    },
+    {
+      name: "NodeJS",
+      value: 75,
+    },
+  ],
+  frontEndData: [
+    {
+      name: "HTML/CSS",
+      value: 95,
+    },
+    {
+      name: "JavaScript",
+      value: 90,
+    },
+    {
+      name: "React",
+      value: 89,
+    },
+    {
+      name: "React Native",
+      value: 90,
+    },
+    {
+      name: "Tailwind",
+      value: 75,
+    },
+    {
+      name: "Sass",
+      value: 85,
+    },
+    {
+      name: "JQuery",
+      value: 80,
+    },
+  ],
+  additionalSkillsData: {
+    withPhoto: [
       {
-        year: '2023',
-        degree: 'Master of Science BIHAR - Big data & AI',
-        place: 'ESTIA(France)\ ITuniversity',
-        attach: [
-          {
-            name: "Estia",
-            logo: "/estia.png",
-            link: "https://www.estia.fr/"
-          },
-          {
-            name: "Bihar",
-            logo: "/bihar.png",
-            link: "https://www.estia.fr/formations/formations-longues-diplomantes/msc-bihar"
-          },
-          {
-            name: "ITU",
-            logo: "/itu.png",
-            link: "https://www.ituniversity-mg.com/",
-            color: "#312999"
-          }
-        ],
+        name: "Visual Code",
+        logo: "/skills/visualcode.png",
       },
       {
-        year: '2021',
-        degree: "Bachelor's degree in development",
-        place: 'ITuniversity',
-        attach: [
-          {
-            name: "ITU",
-            logo: "/itu.png",
-            link: "https://www.ituniversity-mg.com/",
-            color: "#312999"
-          }
-        ],
+        name: "Expo",
+        logo: "/skills/expo.png",
       },
       {
-        year: '2018',
-        degree: "Scientific baccalaureate diploma C",
-        place: 'Lycée Saint Antoine'
+        name: "Github",
+        logo: "/skills/github.png",
+      },
+      {
+        name: "Docker",
+        logo: "/skills/docker.png",
+      },
+      {
+        name: "Firebase",
+        logo: "/skills/firebase.png",
+      },
+      {
+        name: "Postman",
+        logo: "/skills/postman.png",
+      },
+      {
+        name: "MySQL",
+        logo: "/skills/mysql.png",
+      },
+      {
+        name: "Postgresql",
+        logo: "/skills/postgresql.png",
+      },
+      {
+        name: "MongoDB",
+        logo: "/skills/mongodb.png",
+      },
+      {
+        name: "NextJS",
+        logo: "/skills/nextjs.png",
+      },
+      {
+        name: "Wordpress",
+        logo: "/skills/wordpress.png",
       },
     ],
-    profile: {
-      email: "raharimanana99@gmail.com",
-      phone: "+261 34 44 077 12",
-      fullname: "Ando Raharimanana",
-      linkedinLink: "https://www.linkedin.com/in/ando-raharimanana-4917581b1/",
-      githubLink: ""
-    }
-  };
+    noPhoto: [
+      {
+        name: "JWT/Basic/Token Auth",
+      },
+      {
+        name: "RESTful APIs",
+      },
+      {
+        name: "JSON APIs",
+      },
+      {
+        name: "ORMs",
+      },
+      {
+        name: "Microservices",
+      },
+      {
+        name: "Responsive Design",
+      },
+      {
+        name: "DOM Manipulation",
+      },
+      {
+        name: "Fecth API/Ajax",
+      },
+      {
+        name: "Package Managers(npm)",
+      },
+      {
+        name: "TypeScript",
+      },
+      {
+        name: "Blockchain",
+      },
+    ],
+  },
+  formation: [
+    {
+      year: "2023",
+      degree: "Master of Science BIHAR - Big data & AI",
+      place: "ESTIA(France) ITuniversity",
+      attach: [
+        {
+          name: "Estia",
+          logo: "/estia.png",
+          link: "https://www.estia.fr/",
+        },
+        {
+          name: "Bihar",
+          logo: "/bihar.png",
+          link: "https://www.estia.fr/formations/formations-longues-diplomantes/msc-bihar",
+        },
+        {
+          name: "ITU",
+          logo: "/itu.png",
+          link: "https://www.ituniversity-mg.com/",
+          color: "#312999",
+        },
+      ],
+    },
+    {
+      year: "2021",
+      degree: "Bachelor's degree in development",
+      place: "ITuniversity",
+      attach: [
+        {
+          name: "ITU",
+          logo: "/itu.png",
+          link: "https://www.ituniversity-mg.com/",
+          color: "#312999",
+        },
+      ],
+    },
+    {
+      year: "2018",
+      degree: "Scientific baccalaureate diploma C",
+      place: "Lycée Saint Antoine",
+    },
+  ],
+  profile: {
+    email: "raharimanana99@gmail.com",
+    phone: "+261 34 44 077 12",
+    fullname: "Ando Raharimanana",
+    linkedinLink: "https://www.linkedin.com/in/ando-raharimanana-4917581b1/",
+    githubLink: "",
+  },
+};
 
-  export default data
+export default data;
